74942e5af001a99fb07cf6b50024a62cf4bb7165
Alexander Lavrov <a.lavrov88@gmail.com>
2023-03-17 22:07:13 +0300

    login layout and profile dialog

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,32 @@
 <template>
   <v-layout>
+
     <app-alerts />
     <app-nav />
 
-    <div v-if="!appIsInitialized" class="app-loader">
-      <v-progress-circular
-        :size="50"
-        color="light-blue-darken-4"
-        indeterminate
-      ></v-progress-circular>
-    </div>
-
-    <div v-if="appIsInitialized" class="main-layout" :class="{ loading: !appIsInitialized }">
-      <v-card class="main-layout-card" :rounded="true">
-        <app-header />
-        <app-main />
-      </v-card>
-    </div>
+    <app-loader v-if="!appIsInitialized" />
+    <app-main v-if="appIsInitialized && isAuthorized" />
+    <app-login v-if="appIsInitialized && !isAuthorized" />
 
   </v-layout>
 </template>
 
 <script setup lang="ts">
-import { useSettingsStore } from './stores/settingsStore';
-import AppHeader from './components/app-header/app-header.vue'
-import AppMain from './components/app-main/app-main.vue'
-import AppNav from './components/app-nav/app-nav.vue'
-import AppAlerts from './components/app-alerts.vue';
 import { computed } from 'vue';
+import { useSettingsStore } from './stores/settingsStore';
+
+import AppMain from './layouts/app-main.vue'
+import AppLogin from './layouts/app-login.vue'
+import AppLoader from './layouts/app-loader.vue'
+import AppAlerts from './components/app-alerts.vue';
+import AppNav from './components/app-nav/app-nav.vue'
 
 const settingsStore = useSettingsStore()
 const appIsInitialized = computed(() => settingsStore.appIsInitialized)
+const isAuthorized = computed(() => settingsStore.isAuthorized)
 
 const initApp = () => {
   settingsStore.initApp()
 }
 initApp()
 </script>
-
-<style scoped>
-.main-layout {
-  width: 500px;
-  margin-top: 40px;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.main-layout > .v-card {
-  border-radius: 10px;
-}
-
-.app-loader {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
